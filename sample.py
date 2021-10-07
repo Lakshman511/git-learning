6a9ff39146787188d11984c0d10e2db00750a514
Arla Lakshmana Rao <55495069+Lakshman511@users.noreply.github.com>
2021-10-07 10:50:59 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,22 @@
+"""
+Creating new data structure.... It should support gradients passing and computation graph
+"""
+class Vensor:
+    def __init__(self,l):
+        self.data = l[:]
+    def __getitem__(self,key):
+        return self.data[key]
+    def __add__(self,other):
+        if(type(other)==type(1)):
+            res = []
+            for i in range(len(self.data)):
+                res.append(self.data[i]+other)
+            return Vensor(res)
+    def __radd__(self,other):
+        if(type(other)==type(1)):
+            res = []
+            for i in range(len(self.data)):
+                res.append(self.data[i]+other)
+            return Vensor(res)
+    def __repr__(self):
+        return f"Vensor({self.data})"
